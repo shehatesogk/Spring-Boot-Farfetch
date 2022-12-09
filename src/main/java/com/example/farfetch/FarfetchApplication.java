579e997fd54d8907d17bc1dc57f4013c8349df2e
ogkkk <takemefire.99@mail.ru>
2022-12-09 17:16:04 +0600

    Creating project on Spring Initializr

---
+++ src/main/java/com/example/farfetch/FarfetchApplication.java
@@ -0,0 +1,13 @@
+package com.example.farfetch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FarfetchApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FarfetchApplication.class, args);
+    }
+
+}

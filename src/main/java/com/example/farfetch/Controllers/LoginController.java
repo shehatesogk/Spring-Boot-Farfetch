28991829326531b10e5fe62b77a22a84708c95f4
ogkkk <takemefire.99@mail.ru>
2022-12-09 18:06:44 +0600

    Adding Controller and Updating Dependencies

---
+++ src/main/java/com/example/farfetch/Controllers/LoginController.java
@@ -0,0 +1,48 @@
+package com.example.farfetch.Controllers;
+
+import com.example.farfetch.Global.GlobalData;
+import com.example.farfetch.Model.Role;
+import com.example.farfetch.Model.User;
+import com.example.farfetch.Repository.RoleRepository;
+import com.example.farfetch.Repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import java.util.ArrayList;
+import java.util.List;
+
+@Controller
+public class LoginController {
+    @Autowired
+    private BCryptPasswordEncoder bCryptPasswordEncoder;
+    @Autowired
+    UserRepository userRepository;
+    @Autowired
+    RoleRepository roleRepository;
+
+    @GetMapping("/login")
+    public String login(){
+        GlobalData.cart.clear();
+        return "login";
+    }
+    @GetMapping("/register")
+    public String registerGet(){
+        return "register";
+    }
+    @PostMapping("/register")
+    public String registerPost(@ModelAttribute("user") User user, HttpServletRequest request) throws ServletException {
+        String password = user.getPassword();
+        user.setPassword(bCryptPasswordEncoder.encode(password));
+        List<Role> roles = new ArrayList<>();
+        roles.add(roleRepository.findById(2).get());
+        user.setRoles(roles);
+        userRepository.save(user);
+        request.login(user.getEmail(), password);
+        return "redirect:/";
+    }
+}
